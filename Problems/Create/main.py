4e9198ac698f1f12a929eb7f06ee661bb1761ad5
Pat Fonseca <30934372+patfonseca@users.noreply.github.com>
2020-09-06 22:16:36 -0300

    Update main.py

--- Problems/Create/main.py
+++ Problems/Create/main.py
@@ -3,7 +3,3 @@ flowers = {'Alex': 'field flowers',
            'Eva': 'artichoke flower',
            'Daniel': 'tulip'}
 print(flowers)
-
-new_dict = {"a": 6, "b": 3}
-new_dict['a'] = new_dict['a'] / new_dict['b']
-print(new_dict['a'] + new_dict['b'])
